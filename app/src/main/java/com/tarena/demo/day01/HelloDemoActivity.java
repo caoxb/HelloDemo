08accd01cf054a43e1015bf53e33ec47360c3dd4
caoxiabo <caoxb5018@126.com>
2019-01-07 08:54:02 +0800

    Context 使用

--- app/src/main/java/com/tarena/demo/day01/HelloDemoActivity.java
+++ app/src/main/java/com/tarena/demo/day01/HelloDemoActivity.java
@@ -1,7 +1,9 @@
 package com.tarena.demo.day01;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class HelloDemoActivity extends AppCompatActivity {
 
@@ -9,5 +11,20 @@ public class HelloDemoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hello_demo);
+        Context context = this;
+        // context.startActivity(null);
+        // context.startService(null);
+        // //?? 权限 位置？？？？
+        // context.getCacheDir();
+        // context.getDir(name, MODE_PRIVATE)
+        // context.getDatabasePath(name)
+        // context.getExternalCacheDir()
+        // context.getExternalFilesDir(type);
+        // context.getSystemService(name)
+
+        // 上下文
+        // Context context
+
+        View testView = new View(context);
     }
 }
