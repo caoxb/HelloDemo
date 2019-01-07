50b02e43409d2e4763225d591c2db0c97333452b
caoxiabo <caoxb5018@126.com>
2019-01-07 08:21:40 +0800

    Initial commit

---
+++ app/src/main/java/com/tarena/demo/day01/HelloDemoActivity.java
@@ -0,0 +1,13 @@
+package com.tarena.demo.day01;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HelloDemoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hello_demo);
+    }
+}
